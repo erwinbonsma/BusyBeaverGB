3070619972f80f8a55371b779b22be75ca0624ec
Erwin Bonsma <eriban@users.sourceforge.net>
2019-01-01 16:40:39 +0100

    Initial revision of Busy Beaver
    Implemented Program class.

---
+++ BusyBeaver/Program.cpp
@@ -0,0 +1,76 @@
+#include "Program.h"
+
+int dx[numDirections] = { 0, 1, 0, -1 };
+int dy[numDirections] = { 1, 0, -1, 0 };
+
+Program::Program(int size) {
+  _size = size;
+
+  for (int x = 0; x < size; x++) {
+    for (int y = 0; y < size; y++) {
+      _program[x][y] = Instruction::Nop;
+    }
+  }
+
+  reset();
+}
+
+void Program::reset() {
+  _x = 0;
+  _y = -1;
+  _dir = Direction::Up;
+  _numSteps = 0;
+
+  for (int i = 0; i < memorySize; i++) {
+    _memory[i] = 0;
+  }
+}
+
+bool Program::step() {
+  int x, y;
+  Direction oldDir;
+
+  do {
+    x = _x + dx[(int)_dir];
+    y = _y + dy[(int)_dir];
+    oldDir = _dir;
+
+    if (x < 0 || y < 0 || x >= _size || y >= _size) {
+      return true;
+    }
+
+    switch (_program[x][y]) {
+      case Instruction::Mem:
+        switch (_dir) {
+          case Direction::Up:
+            _memory[_ptr]++;
+            break;
+          case Direction::Down:
+            _memory[_ptr]--;
+            break;
+          case Direction::Right:
+            _ptr++;
+            break;
+          case Direction::Left:
+            _ptr--;
+            break;
+        }
+        break;
+
+      case Instruction::Blk:
+        if (_memory[_ptr] > 0) {
+          _dir = (Direction)(((int)_dir + 1) % 4);
+        } else {
+          _dir = (Direction)(((int)_dir + 3) % 4);
+        }
+        break;
+    }
+  } while (_dir != oldDir);
+
+  _numSteps++;
+  _x = x;
+  _y = y;
+
+  return false;
+}
+
---
+++ BusyBeaver/Program.h
@@ -0,0 +1,57 @@
+
+
+enum class Instruction : int {
+  Nop = 0,
+  Blk = 1,
+  Mem = 2
+};
+
+enum class Direction : int {
+  Up = 0,
+  Right = 1,
+  Down = 2,
+  Left = 3
+};
+
+const int maxProgramSize = 9;
+const int memorySize = 64;
+const int numDirections = 4;
+
+class Program {
+  Instruction _program[maxProgramSize][maxProgramSize];
+  int _size;
+
+  // Address pointer
+  int _x, _y;
+  Direction _dir;
+
+  // Memory pointer
+  int _ptr;
+  int _memory[memorySize];
+
+  int _numSteps;
+
+public:
+  Program(int size);
+
+  //void setProgram(int** program);
+
+  Instruction getInstruction(int x, int y) { return _program[x][y]; }
+  void setInstruction(int x, int y, Instruction i) { _program[x][y] = i; }
+
+  int getAddressX() { return _x; }
+  int getAddressY() { return _y; }
+  Direction getDirection() { return _dir; }
+
+  // Resets the program
+  void reset();
+
+  int getMemoryPtr() { return _ptr; }
+  int getMemory(int address) { return _memory[address]; }
+
+  // Executes one instruction. Returns "true" when done
+  bool step();
+
+  int numSteps() { return _numSteps; }
+};
+
