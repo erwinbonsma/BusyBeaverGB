200b0b22af060f8c1162cf30f33ca477bf40096f
Erwin Bonsma <eriban@users.sourceforge.net>
2019-01-19 20:50:04 +0100

    Minor fix to Save implementation
    Updated SAVEBLOCK_NUM to reflect programStoreSize setting in Store.cpp.

--- GoBeaverGo/config-gamebuino.h
+++ GoBeaverGo/config-gamebuino.h
@@ -4,8 +4,8 @@
  * Copyright 2019, Erwin Bonsma
  */
 
-// Should match 16 + maxProgramsToStore in Store.cpp
-#define SAVEBLOCK_NUM 24
+// Should match 16 + programStorageSize in Store.cpp
+#define SAVEBLOCK_NUM 32
 
 // Should match storedProgramSize in Store.cpp
 #define SAVECONF_DEFAULT_BLOBSIZE 17
